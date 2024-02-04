26079ba1784937438bab9a9858161d8a31b2703d
absolutedogy <158856153+absolutedogy@users.noreply.github.com>
2024-02-04 18:09:24 -0500

    Pull hostname from cvar, add template string (#1)
    * Update to use CVAR for server name
    
    Also cleaned up the code slightly to make it more clear what the pattern in the validation is doing
    
    * Updated release DLL with fix
    
    * Updated to support string interpolations
    
    Instead of assuming how the message should be the configuration is updated so that it will take in a string template, which has a supported dictionary of values to be interpolated, and will apply them in the selected spots allowing for customization of the message being generated
    
    * Update README for Clarity and Detail
    
    1. Revised the introduction for better readability.
    2. Enhanced the usage instructions to be more user-friendly.
    3. Provided detailed instructions for each placeholder used in the MessageTemplate.
    4. Added clearer explanation for the JSON configuration file.
    5. Simplified instructions on how to get a Discord webhook.
    6. Elaborated on the message to be seen in Discord when a report is sent.
    
    * Fix Potential Error in MessageInterpolation
    
    Added a boundary check in the string interpolation function to prevent potential out-of-bounds error when detecting escape sequence.
    
    * Updated DLL
    
    * Fix value not binding properly

---
+++ MessageInterpolation.cs
@@ -0,0 +1,62 @@
+﻿namespace ReportDcPlugin;
+
+public class MessageInterpolation
+{
+
+    public static string InterpolateString(string template, Dictionary<string, string> context)
+    {
+        var inToken = false;
+        var result = "";
+        var currToken = "";
+
+        for (int i = 0; i < template.Length; i++)
+        {
+            if (template[i] == '{')
+            {
+                // Handle escape sequence in case the template contains a { in the final message
+                if (i + 1 < template.Length && template[i + 1] == '{')
+                {
+                    i++;
+                    result += "{";
+                }
+                else
+                {
+                    inToken = true;
+                }
+                continue;
+            }
+
+            if (template[i] == '}' && inToken)
+            {
+                inToken = false;
+
+                // If we find the key in our context, ignoring case, append it to the result
+                if (context.Keys.Count(k => string.Compare(k.Trim(), currToken.Trim(), StringComparison.OrdinalIgnoreCase) == 0) > 0)
+                {
+                    result += context[currToken];
+                }
+                else
+                {
+                    // Append a ? to indicate that something should have been placed here but for some reason wasn't
+                    result += "?";
+                }
+                
+                currToken = "";
+                continue;
+            }
+            
+            if (inToken)
+            {
+                currToken += template[i];
+            }
+            else
+            {
+                result += template[i];
+            }
+
+        }
+
+        return result;
+    }
+}
+
